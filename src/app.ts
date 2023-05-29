f313c6053eb74d021732232becde3c04c18e1079
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-05-29 12:04:50 +0600

    ready request response

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response, urlencoded } from "express";
+import cors from 'cors'
 const app: Application = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.use(cors())
+
+app.use(express.json());
+app.use( express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working successfully");
 });
 
 export default app
