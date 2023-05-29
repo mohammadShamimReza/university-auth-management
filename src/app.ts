46a0b30ed87d755b61f35a444f4834b629b28e26
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-05-29 16:17:19 +0600

    testing huskey

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
-import express, { Application, Request, Response, urlencoded } from "express";
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app: Application = express();
+const app: Application = express()
 
+         app.use(cors())
 
-app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json());
-app.use( express.urlencoded({ extended: true }));
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working request response successfully");
-});
+app.get('/', (req: Request, res: any) => {
+  res.send('Working request response successfully')
+})
 
 export default app
