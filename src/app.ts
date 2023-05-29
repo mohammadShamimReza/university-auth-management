d713476656f17f20215e0e42ecb9dd2325177f76
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2023-05-29 12:05:44 +0600

    [DONE] Setup express, mongoose ,cors, parser

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use( express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Working successfully");
+  res.send("Working request response successfully");
 });
 
 export default app
